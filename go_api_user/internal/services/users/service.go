3e2563cd79692f2b028236c93bf1978b64903bc1
amalg01 <gourriamal@gmail.com>
2023-12-13 09:08:17 +0100

    modification de /services/users/service.go

---
+++ go_api_user/internal/services/users/service.go
@@ -0,0 +1,53 @@
+package users
+
+import (
+	"database/sql"
+	"github.com/gofrs/uuid"
+	"github.com/sirupsen/logrus"
+	"middleware/example/internal/models"
+	repository "middleware/example/internal/repositories/users"
+	"net/http"
+)
+
+type UserService struct {
+    db *sql.DB
+}
+
+func NewUserService(db *sql.DB) *UserService {
+    return &UserService{db: db}
+}
+
+func GetAllUsers() ([]models.User, error) {
+	var err error
+	// calling repository
+	users, err := repository.GetAllUsers()
+	// managing errors
+	if err != nil {
+		logrus.Errorf("error retrieving users : %s", err.Error())
+		return nil, &models.CustomError{
+			Message: "Something went wrong",
+			Code:    500,
+		}
+	}
+
+	return users, nil
+}
+
+func GetUserById(id uuid.UUID) (*models.User, error) {
+	user, err := repository.GetUserById(id)
+	if err != nil {
+		if err.Error() == sql.ErrNoRows.Error() {
+			return nil, &models.CustomError{
+				Message: "user not found",
+				Code:    http.StatusNotFound,
+			}
+		}
+		logrus.Errorf("error retrieving users : %s", err.Error())
+		return nil, &models.CustomError{
+			Message: "Something went wrong",
+			Code:    500,
+		}
+	}
+
+	return user, err
+}
